3a2c98af53415df1f5c0178f63ab50d4802810f7
stevenstank <stevenstank775@gmail.com>
2026-04-01 21:24:59 +0530

    chore: remove build files from tracking (dist)

--- apps/backend/dist/controllers/health.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getHealth = getHealth;
-function getHealth(_req, res) {
-    res.status(200).json({
-        success: true,
-        message: "Server is running",
-    });
-}
